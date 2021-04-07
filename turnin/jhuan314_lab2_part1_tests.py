f76a2da154b226c7e036b2ad9a4398245ec97fcc
Jack Huang <jhuan314@wch136-24.cs.ucr.edu>
2021-04-06 17:29:14 -0700

    oops

--- turnin/jhuan314_lab2_part1_tests.py
+++ turnin/jhuan314_lab2_part1_tests.py
@@ -1,35 +1,43 @@
-/*	Author: jhuan314
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
-
-int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRA = 0x00;
-    DDRB = 0xFF;
-    PORTA = 0xFF;
-    PORTB = 0x00;
-    unsigned char tmpA = 0x00;
-    unsigned char tmpB = 0x00;
-    /* Insert your solution below */
-    while (1) {
-	tmpA = PINA & 0x01;
-	tmpB = (PINA & 0x02)>>1;
-	if(tmpA && !tmpB){
- 		PORTB = 1;	
-	}
-	else{
-		PORTB = 0;
-	}
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ {'description': 'Testing when PA1 and PA0 are both 0',
+    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x00)],
+    },
+    {'description': 'Testing when PA1 is 0 and PA0 is 1.',
+    'steps': [ {'inputs': [('PINA', 0x01)],'iterations': 5}],
+    'expected': [('PORTB',0x01)],
+    },
+    {'description': 'Testing when PA1 is 1 and PA0 is 0.',
+    'steps': [ {'inputs': [('PINA', 0x02)],'iterations': 5}],
+    'expected': [('PORTB',0x00)],
+    },
+    {'description': 'Testing when PA1 is 1 and PA0 is 1.',
+    'steps': [ {'inputs': [('PINA', 0x03)],'iterations': 5}],
+    'expected': [('PORTB',0x00)],
+    },
+    {'description': 'Testing random inputs in PA.',
+    'steps': [ {'inputs': [('PINA', 0xF4)],'iterations': 5}],
+    'expected': [('PORTB',0x00)],
     }
-    return 1;
-}
+    ]
+
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+#watch = ['PORTB']
+
